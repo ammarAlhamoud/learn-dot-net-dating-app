98d99a50c58213319c0c095266d02f1fcc16be40
Ammar HM <ammar1007@gmail.com>
2024-01-14 13:02:39 +0100

    setup client with angular , typsScript, Bootstrap and Font Awesome

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -22,6 +22,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
